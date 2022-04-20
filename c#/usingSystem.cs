01b423b129e4f21e68210a073396cb72549d112a
scirpy <102015792+stires@users.noreply.github.com>
2022-04-20 21:32:03 +0300

    print using system
    preview of the start of c++ files..
    this is the beginning of something great.

---
+++ c#/usingSystem.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace HelloThere
+{
+    static void Main()
+    {
+        console.WriteLine("Hello There!");
+        console.WriteLine("I'm hoping you are doing well");
+        console.WriteLine("I'm glad you are working/reading through these files..");
+        console.WriteLine("Alright, bye..")
+    }
+}
