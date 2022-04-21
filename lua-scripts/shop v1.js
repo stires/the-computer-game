45a409f7d15d5b6a746e8ffee59ba9c4fa7036d2
scirpy <vashyre.yt@gmail.com>
2022-04-21 20:22:39 +0300

    new shop update!! (with a patch fixture on the slash commands)

---
+++ lua-scripts/shop v1.js
@@ -0,0 +1,33 @@
+
+//crazy shop system though
+var shop;
+
+ shop = true
+
+var shopItems = ['fish', 'robux', 'cheatsheet', 'gaming god techniques']
+
+let itemPrice = true
+
+var currency;
+ currency = '50 scircoins'
+
+shopItems[1] = itemPrice = `${currency}`
+itemPrice = `${currency}`
+console.log(itemPrice)
+
+const concept = ['this', 'is', 'really', 'cool'];
+
+function changeArr(arr){
+  arr[3] = 'EPIC';
+}
+
+changeArr(concept);
+
+console.log(concept)
+function removeElement (newArr){
+  newArr.pop()
+}
+
+removeElement(concept)
+
+console.log(concept)
