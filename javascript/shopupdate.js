ef06fe66c9ab1c145489f20ecef2a27a51d32791
scirpy_ <102015792+stires@users.noreply.github.com>
2022-04-27 10:00:44 +0300

    updated shop (v2)
    heh

---
+++ javascript/shopupdate.js
@@ -0,0 +1,25 @@
+
+//shop 
+//ver 2
+
+var shop
+
+let shop = true 
+
+const game
+let game = true // i dont care if i get an error actually
+
+
+if (shop){
+    console.log('theres a shop and its working')
+} else { 
+    console.log('there is no shop or it is just not working')
+}
+
+const shopItems = function(items, price){
+    return true, false
+}
+
+const items = function(gosha, gyan, fish, lean, ben){
+    return true //yeah sure i don't care
+}
