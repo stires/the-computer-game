cb0c9f4106cea06e6a0618afb6864a5ec5034943
scirpy_ <102015792+stires@users.noreply.github.com>
2022-04-28 20:00:58 +0300

    scirpys day
    scripys day so far as of 4/28/22

---
+++ javascript/scirpysDay.js
@@ -0,0 +1,44 @@
+
+var scirpy
+
+var scirpysMood = ['happy', 'sad']
+
+const callScirpysMood = function(){
+    console.log(`today, im feeling ${scirpysMood[0]} thank you`)
+}
+
+callScirpysMood()
+
+var scirpysBreakfast = ['tea', 'muffin',  'a bowl of fruits', 'a bowl of cereal', 'fried egg']
+
+const callScirpysBreakfast = function(){
+    console.log(`so today, i had a ${scirpysBreakfast[1]} with a cup of ${scirpysBreakfast[0]}, ${scirpysBreakfast[2]}, ${scirpysBreakfast[3]}, and a ${scirpysBreakfast[4]} to wrap it up.`)
+}
+
+callScirpysBreakfast()
+
+var scirpysLunch = ['pizza', 'soda', 'dessert']
+
+const slicesOfPizza = `5 slices`
+
+const callScirpysLunch = function(){
+    console.log(`for today's lunch, i had ${slicesOfPizza} of ${scirpysLunch[0]}, a 500ml ${scirpysLunch[1]}, and a slice of cake as ${scirpysLunch[2]}. `)
+}
+
+callScirpysLunch()
+
+var scirpysDayOverall = ['bad', 'good', 'ok', 'decent', 'great', 'best in a while']
+
+const callScirpysRating = function(){
+    console.log(`overall, today has been really ${scirpysDayOverall[4]}. im hoping the same for tomorrow.`)
+}
+
+callScirpysRating()
+
+var thankYou = ['thank you']
+
+const thankYouFunction = function(){
+    console.log(`${thankYou[0]} for looking at how my typical day is. bye! :)`)
+}
+
+thankYouFunction()
