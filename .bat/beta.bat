fcb5c3fe6e2bae52d41e1e15ef135bce085c5cfe
scirpy_ <102015792+stires@users.noreply.github.com>
2022-05-02 19:40:42 +0300

    .bat config

---
+++ .bat/beta.bat
@@ -0,0 +1,247 @@
+
+set OUTPUT_FILE-c:\foo\barren.txt 
+set OUTPUT_FILE-c:\foo\thecomputergame.exe 
+@echo off
+  ::this is beta of the windows compability
+@echo on
+::run this a couple times to make sure it works
+
+
+set OUTPUT_FILE-c:\foo\barren.txt 
+set OUTPUT_FILE-c:\foo\thecomputergame.exe 
+@echo off
+  ::this is beta of the windows compability
+@echo on
+set OUTPUT_FILE-c:\foo\barren.txt 
+set OUTPUT_FILE-c:\foo\thecomputergame.exe 
+@echo off
+  ::this is beta of the windows compability
+@echo on
+set OUTPUT_FILE-c:\foo\barren.txt 
+set OUTPUT_FILE-c:\foo\thecomputergame.exe 
+@echo off
+  ::this is beta of the windows compability
+@echo on
+set OUTPUT_FILE-c:\foo\barren.txt 
+set OUTPUT_FILE-c:\foo\thecomputergame.exe 
+@echo off
+  ::this is beta of the windows compability
+@echo on
+set OUTPUT_FILE-c:\foo\barren.txt 
+set OUTPUT_FILE-c:\foo\thecomputergame.exe 
+@echo off
+  ::this is beta of the windows compability
+@echo on
+set OUTPUT_FILE-c:\foo\barren.txt 
+set OUTPUT_FILE-c:\foo\thecomputergame.exe 
+@echo off
+  ::this is beta of the windows compability
+@echo on
+set OUTPUT_FILE-c:\foo\barren.txt 
+set OUTPUT_FILE-c:\foo\thecomputergame.exe 
+@echo off
+  ::this is beta of the windows compability
+@echo on
+set OUTPUT_FILE-c:\foo\barren.txt 
+set OUTPUT_FILE-c:\foo\thecomputergame.exe 
+@echo off
+  ::this is beta of the windows compability
+@echo on
+set OUTPUT_FILE-c:\foo\barren.txt 
+set OUTPUT_FILE-c:\foo\thecomputergame.exe 
+@echo off
+  ::this is beta of the windows compability
+@echo on
+set OUTPUT_FILE-c:\foo\barren.txt 
+set OUTPUT_FILE-c:\foo\thecomputergame.exe 
+@echo off
+  ::this is beta of the windows compability
+@echo on
+set OUTPUT_FILE-c:\foo\barren.txt 
+set OUTPUT_FILE-c:\foo\thecomputergame.exe 
+@echo off
+  ::this is beta of the windows compability
+@echo on
+set OUTPUT_FILE-c:\foo\barren.txt 
+set OUTPUT_FILE-c:\foo\thecomputergame.exe 
+@echo off
+  ::this is beta of the windows compability
+@echo on
+set OUTPUT_FILE-c:\foo\barren.txt 
+set OUTPUT_FILE-c:\foo\thecomputergame.exe 
+@echo off
+  ::this is beta of the windows compability
+@echo on
+set OUTPUT_FILE-c:\foo\barren.txt 
+set OUTPUT_FILE-c:\foo\thecomputergame.exe 
+@echo off
+  ::this is beta of the windows compability
+@echo on
+set OUTPUT_FILE-c:\foo\barren.txt 
+set OUTPUT_FILE-c:\foo\thecomputergame.exe 
+@echo off
+  ::this is beta of the windows compability
+@echo on
+set OUTPUT_FILE-c:\foo\barren.txt 
+set OUTPUT_FILE-c:\foo\thecomputergame.exe 
+@echo off
+  ::this is beta of the windows compability
+@echo on
+set OUTPUT_FILE-c:\foo\barren.txt 
+set OUTPUT_FILE-c:\foo\thecomputergame.exe 
+@echo off
+  ::this is beta of the windows compability
+@echo on
+set OUTPUT_FILE-c:\foo\barren.txt 
+set OUTPUT_FILE-c:\foo\thecomputergame.exe 
+@echo off
+  ::this is beta of the windows compability
+@echo on
+set OUTPUT_FILE-c:\foo\barren.txt 
+set OUTPUT_FILE-c:\foo\thecomputergame.exe 
+@echo off
+  ::this is beta of the windows compability
+@echo on
+set OUTPUT_FILE-c:\foo\barren.txt 
+set OUTPUT_FILE-c:\foo\thecomputergame.exe 
+@echo off
+  ::this is beta of the windows compability
+@echo on
+set OUTPUT_FILE-c:\foo\barren.txt 
+set OUTPUT_FILE-c:\foo\thecomputergame.exe 
+@echo off
+  ::this is beta of the windows compability
+@echo on
+set OUTPUT_FILE-c:\foo\barren.txt 
+set OUTPUT_FILE-c:\foo\thecomputergame.exe 
+@echo off
+  ::this is beta of the windows compability
+@echo on
+set OUTPUT_FILE-c:\foo\barren.txt 
+set OUTPUT_FILE-c:\foo\thecomputergame.exe 
+@echo off
+  ::this is beta of the windows compability
+@echo on
+set OUTPUT_FILE-c:\foo\barren.txt 
+set OUTPUT_FILE-c:\foo\thecomputergame.exe 
+@echo off
+  ::this is beta of the windows compability
+@echo on
+set OUTPUT_FILE-c:\foo\barren.txt 
+set OUTPUT_FILE-c:\foo\thecomputergame.exe 
+@echo off
+  ::this is beta of the windows compability
+@echo on
+set OUTPUT_FILE-c:\foo\barren.txt 
+set OUTPUT_FILE-c:\foo\thecomputergame.exe 
+@echo off
+  ::this is beta of the windows compability
+@echo on
+set OUTPUT_FILE-c:\foo\barren.txt 
+set OUTPUT_FILE-c:\foo\thecomputergame.exe 
+@echo off
+  ::this is beta of the windows compability
+@echo on
+set OUTPUT_FILE-c:\foo\barren.txt 
+set OUTPUT_FILE-c:\foo\thecomputergame.exe 
+@echo off
+  ::this is beta of the windows compability
+@echo on
+set OUTPUT_FILE-c:\foo\barren.txt 
+set OUTPUT_FILE-c:\foo\thecomputergame.exe 
+@echo off
+  ::this is beta of the windows compability
+@echo on
+set OUTPUT_FILE-c:\foo\barren.txt 
+set OUTPUT_FILE-c:\foo\thecomputergame.exe 
+@echo off
+  ::this is beta of the windows compability
+@echo on
+set OUTPUT_FILE-c:\foo\barren.txt 
+set OUTPUT_FILE-c:\foo\thecomputergame.exe 
+@echo off
+  ::this is beta of the windows compability
+@echo on
+set OUTPUT_FILE-c:\foo\barren.txt 
+set OUTPUT_FILE-c:\foo\thecomputergame.exe 
+@echo off
+  ::this is beta of the windows compability
+@echo on
+set OUTPUT_FILE-c:\foo\barren.txt 
+set OUTPUT_FILE-c:\foo\thecomputergame.exe 
+@echo off
+  ::this is beta of the windows compability
+@echo on
+set OUTPUT_FILE-c:\foo\barren.txt 
+set OUTPUT_FILE-c:\foo\thecomputergame.exe 
+@echo off
+  ::this is beta of the windows compability
+@echo on
+set OUTPUT_FILE-c:\foo\barren.txt 
+set OUTPUT_FILE-c:\foo\thecomputergame.exe 
+@echo off
+  ::this is beta of the windows compability
+@echo on
+set OUTPUT_FILE-c:\foo\barren.txt 
+set OUTPUT_FILE-c:\foo\thecomputergame.exe 
+@echo off
+  ::this is beta of the windows compability
+@echo on
+set OUTPUT_FILE-c:\foo\barren.txt 
+set OUTPUT_FILE-c:\foo\thecomputergame.exe 
+@echo off
+  ::this is beta of the windows compability
+@echo on
+set OUTPUT_FILE-c:\foo\barren.txt 
+set OUTPUT_FILE-c:\foo\thecomputergame.exe 
+@echo off
+  ::this is beta of the windows compability
+@echo on
+set OUTPUT_FILE-c:\foo\barren.txt 
+set OUTPUT_FILE-c:\foo\thecomputergame.exe 
+@echo off
+  ::this is beta of the windows compability
+@echo on
+set OUTPUT_FILE-c:\foo\barren.txt 
+set OUTPUT_FILE-c:\foo\thecomputergame.exe 
+@echo off
+  ::this is beta of the windows compability
+@echo on
+set OUTPUT_FILE-c:\foo\barren.txt 
+set OUTPUT_FILE-c:\foo\thecomputergame.exe 
+@echo off
+  ::this is beta of the windows compability
+@echo on
+set OUTPUT_FILE-c:\foo\barren.txt 
+set OUTPUT_FILE-c:\foo\thecomputergame.exe 
+@echo off
+  ::this is beta of the windows compability
+@echo on
+set OUTPUT_FILE-c:\foo\barren.txt 
+set OUTPUT_FILE-c:\foo\thecomputergame.exe 
+@echo off
+  ::this is beta of the windows compability
+@echo on
+set OUTPUT_FILE-c:\foo\barren.txt 
+set OUTPUT_FILE-c:\foo\thecomputergame.exe 
+@echo off
+  ::this is beta of the windows compability
+@echo on
+set OUTPUT_FILE-c:\foo\barren.txt 
+set OUTPUT_FILE-c:\foo\thecomputergame.exe 
+@echo off
+  ::this is beta of the windows compability
+@echo on
+set OUTPUT_FILE-c:\foo\barren.txt 
+set OUTPUT_FILE-c:\foo\thecomputergame.exe 
+@echo off
+  ::this is beta of the windows compability
+@echo on
+set OUTPUT_FILE-c:\foo\barren.txt 
+set OUTPUT_FILE-c:\foo\thecomputergame.exe 
+@echo off
+  ::this is beta of the windows compability
+@echo on
+
+@echo off
+::seems sufficient
