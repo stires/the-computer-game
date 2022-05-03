89fb33dee4f210694fb5d84fe754696a71d3355e
scirpy_ <102015792+stires@users.noreply.github.com>
2022-05-03 08:38:12 +0300

    website data log
    a

---
+++ javascript/website-data.js
@@ -0,0 +1,35 @@
+
+var website = ['website', 'why i made this website']
+
+const callArray = function(website, array){
+    console.log(`${website[0]}`)
+    website = true;
+    array === 'website', 'why i made this website' 
+    array = true, array = false
+    return true, false
+}
+
+const callOtherArray = function(website, array){
+    console.log(`${website[1]}`)
+    website = true in pairs; 
+    website = false; //else 
+    array === 'website' && 'why i made this website'
+    array = true 
+    console.log(array) //checks the value of array
+    return true;
+}
+
+callArray()
+callOtherArray(website, array)
+
+if (callArray() && callOtherArray()){
+    console.log('arrays work!!!')
+} else if (callArray()){
+    console.log(`array don't work fully :skull:`)
+} else if (callOtherArray()){
+    console.log(`tad bit of an oopsies, the arrays arent operational`)
+} else {
+    console.log('nothing is working')
+}
+
+//ok
